e2c1f1d37f7bddbe88046779c542110ab624a9c9
Naved <naved1234@gmail.com>
2018-07-23 14:38:07 +0530

    code

--- material-prod/src/app/students/services/student.service.ts
+++ material-prod/src/app/students/services/student.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs/Observable";
+import {Student} from "../models/student";
+
+const BASE_URL = 'http://localhost:3000/api';
 
 @Injectable()
 export class StudentService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  getStudents(): Observable<Student[]> {
+    return this.httpClient.get<Student[]>(`${BASE_URL}/students`);
+  }
 
 }
