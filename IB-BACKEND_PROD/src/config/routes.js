2a81247c19a98dec14e32143a097d6d970cde7c3
Naved <naved1234@gmail.com>
2018-07-24 21:17:06 +0530

    code

--- IB-BACKEND_PROD/src/config/routes.js
+++ IB-BACKEND_PROD/src/config/routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
 import studentController from '../api/controllers/student.controller';
+import passport from 'passport';
 
 export const router = express.Router();
 
 // Students
-router.get('/students', studentController.findAll);
-router.get('/students/:id', studentController.findOne);
-router.delete('/students/:id', studentController.delete);
-router.put('/students/:id', studentController.update);
-router.post('/students', studentController.create);
+router.get('/students', passport.authenticate('jwt', {session: false}), studentController.findAll);
+router.get('/students/:id', passport.authenticate('jwt', {session: false}), studentController.findOne);
+router.delete('/students/:id', passport.authenticate('jwt', {session: false}), studentController.delete);
+router.put('/students/:id', passport.authenticate('jwt', {session: false}), studentController.update);
+router.post('/students', passport.authenticate('jwt', {session: false}), studentController.create);
