3327da2bacf347c05a5432a2e2b31a9125431ac5
Naved <naved1234@gmail.com>
2018-07-25 09:24:08 +0530

    code

--- material-prod/src/app/dashboard/components/toolbar/toolbar.component.ts
+++ material-prod/src/app/dashboard/components/toolbar/toolbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import {JwtService} from "../../../core/services/jwt.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-toolbar',
@@ -8,9 +10,15 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 export class ToolbarComponent implements OnInit {
   @Output()
   toggleSidenav = new EventEmitter<void>();
-  constructor() { }
+  constructor(private jwtService: JwtService,
+              private router: Router) { }
 
   ngOnInit() {
+  }
+
+  logout() {
+    this.jwtService.destroyToken();
+    this.router.navigate(['/login']);
   }
 
 }
